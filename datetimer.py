98fefa2a621a1625bcd6384c8c0f45db2a697f92
Emiliano Haag <emilianohaag10@gmail.com>
2022-03-27 11:49:09 -0300

    Comments added

--- datetimer.py
+++ datetimer.py
@@ -1,12 +1,10 @@
 from datetime import datetime, timedelta
 
-#example = "12/03/22"
-
-
+#receive a date like "12/03/22"
+#return true if date is passed over 7 days
 def passedSevenDays(v):
     sevenDays =datetime.now() - timedelta(days=7)
     ceTime = datetime.strptime(v, "%d/%m/%y")
     print("is " , ceTime , " minor than ", sevenDays , " ? ", ceTime < sevenDays)
     return ceTime < sevenDays
 
-#print(passedSevenDays(example))
